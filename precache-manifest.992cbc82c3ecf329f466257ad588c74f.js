8e8876476c7c7ab57ae2d8fa51101359ed93a64c
Alex Hughes <alexhughes@AlexMBPro.local>
2020-02-07 11:47:18 -0800

    Updates

---
+++ precache-manifest.992cbc82c3ecf329f466257ad588c74f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c19f9b450cb16e401037d5e69031e085",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22f735d9f6c442fb5435",
+    "url": "/static/css/main.bc7feea9.chunk.css"
+  },
+  {
+    "revision": "59a3424ec70bbc92dd5b",
+    "url": "/static/js/2.0cfec085.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0cfec085.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22f735d9f6c442fb5435",
+    "url": "/static/js/main.61ba0c27.chunk.js"
+  },
+  {
+    "revision": "a0f31eeb5b15188c2764",
+    "url": "/static/js/runtime-main.8984fa7e.js"
+  },
+  {
+    "revision": "845b120529ddfdd06fc2925327aab822",
+    "url": "/static/media/FroshsBarberShopLogo.845b1205.png"
+  },
+  {
+    "revision": "09917d019e987ab889d9044164f17ca0",
+    "url": "/static/media/barberChair.09917d01.jpg"
+  },
+  {
+    "revision": "af118a2907844d1556e35a4101aebe16",
+    "url": "/static/media/button.af118a29.png"
+  },
+  {
+    "revision": "50a0344945c9df2a67b60ef32ee43a0f",
+    "url": "/static/media/mailboxes.50a03449.jpg"
+  },
+  {
+    "revision": "dd3776b3cdaafca43d9d974fe63dd8f8",
+    "url": "/static/media/razorIcon.dd3776b3.jpg"
+  },
+  {
+    "revision": "fe36dcb762f98a2dd3dbab84be63c3f6",
+    "url": "/static/media/scissorIcon.fe36dcb7.jpg"
+  },
+  {
+    "revision": "fd47904f2b15759d5e83bb97359d0485",
+    "url": "/static/media/woodTexture.fd47904f.jpg"
+  }
+]);
